f8271adf732aed181b15cce6e7f99c8b86d801df
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-05-21 08:00:13 -0600

    Commit
    En este commit agrego todos los cambios al lab

---
+++ Assets/FlashLight.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashLight : MonoBehaviour
+{
+    public Light spotLight;
+    public AudioSource audio;
+    public Canvas canv;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spotLight.enabled = false;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            
+            if (spotLight)
+            {
+                if (spotLight.enabled)
+                {
+                    spotLight.enabled = false;
+                }
+                else
+                {
+                    spotLight.enabled = true;
+                }
+            
+            }
+
+            if (audio)
+            {
+                audio.Play();
+            }
+          
+        }
+         //se crea un rayo en el centro de la camara para poder determinar el nombre de los objetos que se apuntan
+         
+        RaycastHit hitInfo;
+
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(myRay,out hitInfo))
+        {
+            if (hitInfo.transform.gameObject.CompareTag("chair"))
+            {
+                canv.GetComponentInChildren<Text>().text = "Silla";
+               
+            }
+            else if (hitInfo.transform.gameObject.CompareTag("table"))
+            {
+                canv.GetComponentInChildren<Text>().text = "Mesa";
+            }
+            else
+            {
+
+                canv.GetComponentInChildren<Text>().text = "";
+            }
+        }
+
+      
+
+    }
+
+ 
+
+}
---
+++ Assets/areaControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class areaControler : MonoBehaviour
+
+{
+
+    public Light ambientLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ambientLight.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Entro");
+        ambientLight.enabled = true;
+        ambientLight.intensity = Mathf.Sin(5*Time.fixedTime);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ambientLight.enabled = false;
+
+    }
+}
